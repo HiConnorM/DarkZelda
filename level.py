7fd48350facc3fe3449e43d4d7d365a0a144a904
Connor Morgan <theconnorm@gmail.com>
2022-05-19 23:40:53 -0500

    basic pngs added

--- level.py
+++ level.py
@@ -1,4 +1,7 @@
 import pygame
+from settings import WORLD_MAP, TILESIZE
+from tile import Tile
+from player import Player
 
 class Level:
     def __init__(self):
@@ -8,6 +11,16 @@ class Level:
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
 
+        self.create_map()
+
+    def create_map(self):
+        for row_index,row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x,y),[self.visible_sprites])
+
     def run(self):
         #update and draw game
-        pass
+        self.visible_sprites.draw(self.display_surface)
