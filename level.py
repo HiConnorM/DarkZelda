025749f4b75fd6ef7f8f81fa3a645ab390abea7a
Connor Morgan <theconnorm@gmail.com>
2022-05-19 22:47:53 -0500

    Initial commit

---
+++ level.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Level:
+    def __init__(self):
+        #get display surface
+        self.display_surface = pygame.display.get_surface()
+        #sprite group
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacles_sprites = pygame.sprite.Group()
+
+    def run(self):
+        #update and draw game
+        pass
