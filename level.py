e017db33472d80caa1da20115ee7f3b77a484a25
Connor Morgan <theconnorm@gmail.com>
2022-05-20 22:44:28 -0500

    added movement to player

--- level.py
+++ level.py
@@ -1,7 +1,8 @@
 import pygame
-from settings import WORLD_MAP, TILESIZE
+from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -19,8 +20,12 @@ class Level:
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == 'x':
-                    Tile((x,y),[self.visible_sprites])
+                    Tile((x,y),[self.visible_sprites, self.obstacles_sprites])
+                if col == 'p':
+                    self.player = Player((x,y),[self.visible_sprites])
 
     def run(self):
         #update and draw game
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
